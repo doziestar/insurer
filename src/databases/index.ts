2029d11171541e4803038352e377ee6c1558ad7d
doziestar <chidosiky2015@gmail.com>
2022-05-27 08:59:29 +0100

    start insurance startup

---
+++ src/databases/index.ts
@@ -0,0 +1,36 @@
+import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USER, NODE_ENV } from '@config';
+import UserModel from '@models/users.model';
+import { logger } from '@utils/logger';
+import Sequelize from 'sequelize';
+
+const sequelize = new Sequelize.Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
+  dialect: 'postgres',
+  host: DB_HOST,
+  port: 5432,
+  timezone: '+09:00',
+  define: {
+    charset: 'utf8mb4',
+    collate: 'utf8mb4_general_ci',
+    underscored: true,
+    freezeTableName: true,
+  },
+  pool: {
+    min: 0,
+    max: 5,
+  },
+  logQueryParameters: NODE_ENV === 'development',
+  logging: (query, time) => {
+    logger.info(time + 'ms' + ' ' + query);
+  },
+  benchmark: true,
+});
+
+sequelize.authenticate();
+
+const DB = {
+  Users: UserModel(sequelize),
+  sequelize, // connection instance (RAW queries)
+  Sequelize, // library
+};
+
+export default DB;
