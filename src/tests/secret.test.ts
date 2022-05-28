e541d74a295c2d1f21db8da541fd2b192171bf03
doziestar <chidosiky2015@gmail.com>
2022-05-28 15:18:34 +0100

    integration test

--- src/tests/secret.test.ts
+++ src/tests/secret.test.ts
@@ -2,15 +2,19 @@ import App from '@/app';
 import APIRoute from '@routes/secret.route';
 import request from 'supertest';
 
+afterAll(async () => {
+  await new Promise<void>(resolve => setTimeout(() => resolve(), 500));
+});
 
-describe("Testing API Keys and API Secrets ", () => {
-  describe("[POST] /secret/new", () => {
-    it("response should have the Create api keys and secret", async () => {
-      const app = App([ new APIRoute()])
-      expect(true).toBe(true);
-      request(app.getServer()).post(`${APIRoute.path}new`, () => {
-        expect(1).toBe(1)
-      }),
+describe('Testing API Keys and API Secrets ', () => {
+  describe('[POST] /secret/new', () => {
+    it('response should have the Created api keys and secret', async () => {
+      const secretRoute = new APIRoute();
+      const app = new App([secretRoute]);
+      return request(app.getServer()).post(`${secretRoute.path}new`, () => {
+        //
+        expect(true).toBe(true);
+      });
     });
   });
 });
